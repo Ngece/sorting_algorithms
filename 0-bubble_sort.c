2287e04f7b99b6eb07595a9260290d342fe11879
Ngece <ngecemlamli@gmail.com>
2023-01-28 18:09:54 -0800

    committing bubble sort changed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,34 @@
 #include "deck.h"
-/**
- *swap_bubble -  swapping function
- *@array: the array
- *@x: the position
- */
-void swap_bubble(int *array, int x)
-{
-	int tmp = array[x];
-
-	array[x] = array[x + 1];
-	array[x + 1] = tmp;
-}
 
 /**
- *bubble_sort - sort a linked list using bubble algo
- *@array: the array to sort
- *@size: the array size
- */
+  * bubble_sort - Sorts an array of integers in ascending
+  * order using the Bubble sort algorithm.
+  * @array: The array of integers to sort
+  * @size: The size of the array of integers
+  *
+  * Return: Nothing!
+  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t  x;
-	int flag = 0;
+	int i = 0, aux = 0, max = 0, swapped = 1;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
-	while (!flag)
-	{
-		flag = 1;
-		for (x = 0; x < size - 1; x++)
-		{
-			if (array[x] > array[x + 1])
-			{
-				swap_bubble(array, x);
-				flag = 0;
-				print_array(array, size);
-			}
-		}
-	}
 
+	max = size - 1;
+
+	for (; i < max; ++i)
+	{
+		if (array[i] > array[i + 1])
+		{
+			aux = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = aux;
+			swapped = 1;
+			print_array(array, size);
+		}
+
+		if (swapped == 1 && i == max - 1)
+			i = -1, swapped = 0, --max;
+	}
 }
